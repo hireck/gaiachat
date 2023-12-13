5381a330c2e55e3cc14c560faef443c0f779f98a
hireck <reckmanhgb@gmail.com>
2023-12-13 09:35:12 +0100

    added password check

--- gaia_chatbot.py
+++ gaia_chatbot.py
@@ -5,10 +5,46 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import streamlit as st
 import openai
-#from streamlit_chat import message
+import hmac
 
-#from dotenv import load_dotenv, find_dotenv
-#load_dotenv(find_dotenv(), override=True)
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("ߘ堕ser not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
 headers = {
     "authorization":st.secrets['OPENAI_API_KEY'],
     "content-type":"application/json"
