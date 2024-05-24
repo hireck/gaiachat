b86e44275f184ee62717b3099b84a6c72f76207b
hireck <reckmanhgb@gmail.com>
2024-05-24 16:56:41 +0200

    switched to L12

--- gaia_chatbot.py
+++ gaia_chatbot.py
@@ -71,8 +71,9 @@ st.title('Gaia chatbot')
 
 @st.cache_data
 def load_vectors():
-    embedding_model = HuggingFaceEmbeddings(model_name="thenlper/gte-large", encode_kwargs={"normalize_embeddings": True},)#"msmarco-bert-base-dot-v5")
-    #embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+    embedding_model =  HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
+    #embedding_model = HuggingFaceEmbeddings(model_name="thenlper/gte-large", encode_kwargs={"normalize_embeddings": True},)#"msmarco-bert-base-dot-v5")
+    #embedding_model =  HuggingFaceEmbeddings(model_name="thenlper/gte-large", encode_kwargs={"normalize_embeddings": True}SentenceTransformer('all-MiniLM-L6-v2')
     #embedmodel.max_seq_length = 512
     return FAISS.load_local("faiss_index_combined", embedding_model, allow_dangerous_deserialization=True)
 
